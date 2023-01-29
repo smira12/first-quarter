bf311f2e9a030a616a1a5e83059b1da77911c972
smira12 <m1ronsa89@gmail.com>
2023-01-30 02:43:45 +0300

    Add the task for the final task

---
+++ Task/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задача: Написать программу которая из имеющегося массива строк формирует массив из
+ строк длина которых меньше, либо равна трём символам. Массив можно задать 
+ на старте, либо ввести пользователем.
+*/
+
+string[] arrayFirst = new string[] { "876", "msa", "hello", "world", "vdv", "12" };
+string[] arraySecond = new string[arrayFirst.Length];
+
+void SecondArray(string[] arrayFirst, string[] arraySecond)
+{
+    int count = 0;
+    for (int i = 0; i < arrayFirst.Length; i++)
+    {
+        if (arrayFirst[i].Length <= 3)
+        {
+            arraySecond[count] = arrayFirst[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(arrayFirst, arraySecond);
+PrintArray(arraySecond);
